dd5c5bb2ff2400a0a8ea60395156d22b822284ef
Alfonso <fofofalcone@gmail.com>
2021-05-11 21:18:26 +0200

    Repush DEVELOP_

---
+++ DEVELOP_/src/js/common.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    /**
+     * Feel free to remove this function!
+     */
+    const testFunction = () => {
+
+        // do something
+        console.log('Testing… 1, 2, 3');
+
+    };
+
+    /**
+     * Chocks away!
+     */
+    testFunction();
+
+});
+
